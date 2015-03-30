ee3fcf9474c8da56a3e43ef7a10331e97559e570
pridexs <alehstk@gmail.com>
2015-03-30 20:25:04 -0300

    Arrumado nome das versoes compiladas

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-import sys
-import requests
-import time
-from lxml import etree
-from lxml import html
-from load_config import ConfigLoader
 
 '''
 Arquivo para testar algumas funcoes diretamente num HTML, sem usar o servidor.
 '''
-
-if __name__ == '__main__':
-    print("Test")
